fc2169f5b5e97345453752573ab2f0c59bd0df97
atikrahman-ew <atikrahman.ew@gmail.com>
2019-02-03 14:39:03 +0600

    Update Route

--- src/routes.php
+++ src/routes.php
@@ -10,7 +10,6 @@ Route::get('/flush', function () {
     Artisan::call('view:clear');
     Artisan::call('route:clear');
     Artisan::call('config:clear');
-    Artisan::call('auth:clear-resets');
     Session::flush();
     return("Cleared all Type Of caches to Ashes !");
 });
